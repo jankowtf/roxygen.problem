4d292fedc1c7e70c5eb11ba5d6ac10ab2783097e
Janko Thyson <janko.thyson@rappster.de>
2014-10-20 14:57:12 +0200

    initial commit

---
+++ R/foo.r
@@ -0,0 +1,42 @@
+#' @title
+#' Foo (generic)
+#'
+#' @description 
+#' Blabla.
+#'     
+#' @param x \strong{Signature argument}.
+#'    Object containing expression information. Typically, a function or an
+#'    "curly expression/call".
+setGeneric(
+  name = "foo",
+  signature = c(
+    "x"
+  ),
+  def = function(
+    x,
+    ...
+  ) {
+    standardGeneric("foo")       
+  }
+)
+
+#' @describeIn foo
+#' @inheritParams foo
+#' @param x \code{\link{{}}}.
+#' @return \code{\link{expression}}. 
+#' @export
+setMethod(
+  f = "foo", 
+  signature = signature(
+    x = "{"
+  ), 
+  definition = function(
+    x,
+    ...
+  ) {
+    
+  print(x)
+  return(TRUE)
+  
+  }
+)
